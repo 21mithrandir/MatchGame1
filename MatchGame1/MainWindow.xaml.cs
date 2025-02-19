4a381452583a421b9d4d4b0fdc42b601b2b3dd7c
21hyl <21hyl@DESKTOP-MOIF5CA>
2025-02-19 02:29:16 -0800

    Add project files.

---
+++ MatchGame1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MatchGame1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        SetUpGame();
+    }
+
+    private void SetUpGame()
+    {
+        List<string> animalEmoji = new List<String>()
+        {
+            "👽" , "👽",
+            "💩" , "💩",
+            "🤖" , "🤖",
+            "🐢" , "🐢",
+            "🦉" , "🦉",
+            "🦋" , "🦋",
+            "🐦‍🔥" , "🐦‍🔥",
+            "🐒" , "🐒",
+        };
+        Random random = new Random();
+
+        foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+        {
+            int index = random.Next(animalEmoji.Count);
+            string nextEmoji = animalEmoji[index];
+            textBlock.Text = nextEmoji;
+            animalEmoji.RemoveAt(index);
+        }
+    }
+}
